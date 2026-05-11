c285d59f67f19f55cad32ae1334472b7a1fef3ab
Jvanesa-1139 <jvanessaec@gmail.com>
2026-05-11 15:16:10 -0500

    elimimo cambios de configuracion local

--- src/main/java/com/project/config/DatabaseConfig.java
+++ src/main/java/com/project/config/DatabaseConfig.java
@@ -11,21 +11,25 @@ public class DatabaseConfig {
 
 
     public static Connection getConnection() throws SQLException {
-
         String host = EnvConfig.get("DB_HOST", "localhost");
+        String port = EnvConfig.get("DB_PORT", "1433");
         String db = EnvConfig.get("DB_NAME");
+        String user = EnvConfig.get("DB_USER");
+        String password = EnvConfig.get("DB_PASSWORD");
 
         String encrypt = EnvConfig.get("DB_ENCRYPT", "true");
         String trustServerCertificate = EnvConfig.get("DB_TRUST_SERVER_CERTIFICATE", "true");
+        String url = "jdbc:sqlserver://" + host + ":" + port + ";"
+                + "databaseName=" + db + ";"
+                + "encrypt=" + encrypt + ";"
+                + "trustServerCertificate=" + trustServerCertificate + ";";
 
-        
-        String url = "jdbc:sqlserver://localhost:1433;"
-                + "databaseName=DBPlanilla;"
-                + "user=sa;"
-                + "password=11391491;"
-                + "encrypt=true;"
-                + "trustServerCertificate=true;";
+        Properties props = new Properties();
 
-        return DriverManager.getConnection(url);
+        props.setProperty("user", user);
+        props.setProperty("password", password);
+
+        return DriverManager.getConnection(url, props);
     }
+
 }
--- src/main/java/com/project/config/EnvConfig.java
+++ src/main/java/com/project/config/EnvConfig.java
@@ -3,35 +3,25 @@ package com.project.config;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class EnvConfig {
+    private static final Dotenv dotenv = Dotenv.configure()
+            .ignoreIfMalformed()
+            .ignoreIfMissing()
+            .load();
 
-	private static final Dotenv dotenv = Dotenv.configure()
-	        .directory(".")
-	        .ignoreIfMalformed()
-	        .ignoreIfMissing()
-	        .load();
 
-	static {
-	    System.out.println("DIRECTORIO: " + System.getProperty("user.dir"));
-	}
+    private EnvConfig() {
+    }
+
     public static String get(String key) {
-
         String systemValue = System.getenv(key);
-
         if (systemValue != null && !systemValue.isBlank()) {
             return systemValue;
         }
-
         return dotenv.get(key);
     }
 
     public static String get(String key, String defaultValue) {
-
-        String systemValue = System.getenv(key);
-
-        if (systemValue != null && !systemValue.isBlank()) {
-            return systemValue;
-        }
-
         return dotenv.get(key, defaultValue);
     }
+
 }
